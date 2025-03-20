57049c41491183b006137db0c175ee3e3847948f
Ubuntu <ubuntu@ip-172-31-38-231.ap-southeast-1.compute.internal>
2025-03-20 07:44:52 +0000

    submit-1742456692172-kafka-feb25-org-akhil-89

--- assignments/kafka_app.py
+++ assignments/kafka_app.py
@@ -6,12 +6,7 @@ def produce_messages(kafka_config, topic, messages):
     """
     Produce messages to the specified Kafka topic.
     """
-    producer = KafkaProducer(bootstrap_servers=kafka_config["bootstrap.servers"],
-                             value_serializer=lambda v: v.encode('utf-8'))
-    for msg in messages:
-        producer.send(topic, value=msg)
-    producer.flush()
-    producer.close()
+
     return True
 
 def consume_messages(kafka_config, topic, num_messages):
@@ -19,20 +14,9 @@ def consume_messages(kafka_config, topic, num_messages):
     Consume a specified number of messages from the given Kafka topic.
     Returns a list of consumed messages.
     """
-    consumer = KafkaConsumer(
-        topic,
-        bootstrap_servers=kafka_config["bootstrap.servers"],
-        auto_offset_reset='earliest',
-        group_id='test-group',
-        value_deserializer=lambda v: v.decode('utf-8'),
-        consumer_timeout_ms=10000  # stops if no messages in 10s
-    )
+
     messages = []
-    for msg in consumer:
-        messages.append(msg.value)
-        if len(messages) >= num_messages:
-            break
-    consumer.close()
+
     return messages
 
 if __name__ == "__main__":
